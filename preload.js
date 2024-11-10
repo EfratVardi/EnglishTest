fa6724590ff610b0953aec622e6352f4d8ad677a
EfratVardi <211.efrat@gmail.com>
2024-11-10 11:00:47 +0200

    in progress

--- preload.js
+++ preload.js
@@ -3,10 +3,10 @@ const electron = require('electron');
 const ipc = require('electron').ipcRenderer
 
 contextBridge.exposeInMainWorld('expose', {
-    SendExcel: (channel, data) => {
+    Send: (channel, data) => {
         ipcRenderer.send(channel, data);
     },
-    ReceiveExcel: (channel, func) => {
+    Receive: (channel, func) => {
         ipcRenderer.on(channel, (event, ...args) => func(...args));
     },
     appClose: () => {
